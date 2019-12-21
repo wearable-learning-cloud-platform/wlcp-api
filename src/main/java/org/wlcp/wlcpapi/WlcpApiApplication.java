fc6fe97a4c558612cc4e878dff3cb0890c7c1869
mmicciolo <mmicciolo@wpi.edu>
2019-12-20 21:23:25 -0500

    inital commit

---
+++ src/main/java/org/wlcp/wlcpapi/WlcpApiApplication.java
@@ -0,0 +1,13 @@
+package org.wlcp.wlcpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlcpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlcpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wlcp/wlcpapi/WlcpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wlcp.wlcpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlcpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
