a1fc050f4eb4e15a06037deb62e1b871ecaad060
mmicciolo <mmicciolo@wpi.edu>
2019-12-20 22:55:16 -0500

    Added dockerfile

--- src/main/java/org/wlcp/wlcpapi/WlcpApiApplication.java
+++ src/main/java/org/wlcp/wlcpapi/WlcpApiApplication.java
@@ -2,8 +2,10 @@ package org.wlcp.wlcpapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WlcpApiApplication {
 
 	public static void main(String[] args) {
