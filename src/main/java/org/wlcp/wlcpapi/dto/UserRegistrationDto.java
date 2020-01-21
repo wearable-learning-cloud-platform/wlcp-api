214bf5f6e3d7dcc9db224fff9be30cda3865f097
mmicciolo <mmicciolo@wpi.edu>
2020-01-20 21:04:25 -0500

    Added very basic user registration. Needs to be refactored eventually.

---
+++ src/main/java/org/wlcp/wlcpapi/dto/UserRegistrationDto.java
@@ -0,0 +1,10 @@
+package org.wlcp.wlcpapi.dto;
+
+public class UserRegistrationDto {
+
+	public String usernameId;
+	public String password;
+	public String firstName;
+	public String lastName;
+	
+}
