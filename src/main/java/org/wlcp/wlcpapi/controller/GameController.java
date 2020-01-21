4f16f66d13ada7f68fd99f2b5af88016fdbc8dde
mmicciolo <mmicciolo@wpi.edu>
2020-01-20 23:00:43 -0500

    Added get games controller.

--- src/main/java/org/wlcp/wlcpapi/controller/GameController.java
+++ src/main/java/org/wlcp/wlcpapi/controller/GameController.java
@@ -1,5 +1,7 @@
 package org.wlcp.wlcpapi.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.wlcp.wlcpapi.datamodel.master.Game;
+import org.wlcp.wlcpapi.dto.GameDto;
+import org.wlcp.wlcpapi.dto.GenericResponse;
 import org.wlcp.wlcpapi.repository.GameRepository;
 
 @Controller
@@ -18,6 +22,18 @@ public class GameController {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@GetMapping("/getGames")
+	public ResponseEntity<GenericResponse> getGames() {
+		List<Game> games = gameRepository.findAll();
+		List<GameDto> returnGames = new ArrayList<GameDto>();
+		for(Game game : games) {
+			if(game.getVisibility() && !game.getDataLog()) {
+				returnGames.add(new GameDto(game.getGameId()));
+			}
+		}
+		return new ResponseEntity<GenericResponse>(new GenericResponse("", returnGames), HttpStatus.OK);
+	}
 	
 	@GetMapping(value="/getGame/{gameId}")
 	public ResponseEntity<Game> getGame(@PathVariable String gameId) {
